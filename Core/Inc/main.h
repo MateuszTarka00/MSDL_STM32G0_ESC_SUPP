0b39a2b4d9c07f35dec66a0eecd43b5aefee3a26
MateuszTarka00 <80099249+MateuszTarka00@users.noreply.github.com>
2026-02-24 12:44:53 +0100

    Initial commit

---
+++ Core/Inc/main.h
@@ -0,0 +1,107 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32c0xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define TEACH_INPUT_Pin GPIO_PIN_3
+#define TEACH_INPUT_GPIO_Port GPIOA
+#define ROTATION_CONTROL_Pin GPIO_PIN_4
+#define ROTATION_CONTROL_GPIO_Port GPIOA
+#define STAND_CONTROL_Pin GPIO_PIN_5
+#define STAND_CONTROL_GPIO_Port GPIOA
+#define SAFETY_END_Pin GPIO_PIN_0
+#define SAFETY_END_GPIO_Port GPIOB
+#define INSPECTION_MODE_Pin GPIO_PIN_1
+#define INSPECTION_MODE_GPIO_Port GPIOB
+#define SPEED_READY_Pin GPIO_PIN_13
+#define SPEED_READY_GPIO_Port GPIOB
+#define TEACH_FAST_Pin GPIO_PIN_14
+#define TEACH_FAST_GPIO_Port GPIOB
+#define IN_FREQ_Pin GPIO_PIN_7
+#define IN_FREQ_GPIO_Port GPIOC
+#define WATCHDOG_OUT_Pin GPIO_PIN_15
+#define WATCHDOG_OUT_GPIO_Port GPIOA
+#define K2_EN_Pin GPIO_PIN_0
+#define K2_EN_GPIO_Port GPIOD
+#define ROTATION_S2_Pin GPIO_PIN_1
+#define ROTATION_S2_GPIO_Port GPIOD
+#define MIS_ST2_Pin GPIO_PIN_2
+#define MIS_ST2_GPIO_Port GPIOD
+#define MIS_ST1_Pin GPIO_PIN_3
+#define MIS_ST1_GPIO_Port GPIOD
+#define TEACH_SLOW_Pin GPIO_PIN_3
+#define TEACH_SLOW_GPIO_Port GPIOB
+#define SAFETY_OK_Pin GPIO_PIN_4
+#define SAFETY_OK_GPIO_Port GPIOB
+#define STAND_OK_Pin GPIO_PIN_5
+#define STAND_OK_GPIO_Port GPIOB
+#define ROTATION_OK_Pin GPIO_PIN_6
+#define ROTATION_OK_GPIO_Port GPIOB
+#define CHECK_UP_Pin GPIO_PIN_8
+#define CHECK_UP_GPIO_Port GPIOB
+#define CHECK_DOWN_Pin GPIO_PIN_9
+#define CHECK_DOWN_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
---
+++ Core/Src/gpio.c
@@ -0,0 +1,117 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    gpio.c
+  * @brief   This file provides code for the configuration
+  *          of all used GPIO pins.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "gpio.h"
+
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/*----------------------------------------------------------------------------*/
+/* Configure GPIO                                                             */
+/*----------------------------------------------------------------------------*/
+/* USER CODE BEGIN 1 */
+
+/* USER CODE END 1 */
+
+/** Configure pins as
+        * Analog
+        * Input
+        * Output
+        * EVENT_OUT
+        * EXTI
+*/
+void MX_GPIO_Init(void)
+{
+
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, INSPECTION_MODE_Pin|SPEED_READY_Pin|SAFETY_OK_Pin|STAND_OK_Pin
+                          |ROTATION_OK_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(WATCHDOG_OUT_GPIO_Port, WATCHDOG_OUT_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(K2_EN_GPIO_Port, K2_EN_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : TEACH_INPUT_Pin ROTATION_CONTROL_Pin STAND_CONTROL_Pin */
+  GPIO_InitStruct.Pin = TEACH_INPUT_Pin|ROTATION_CONTROL_Pin|STAND_CONTROL_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SAFETY_END_Pin TEACH_FAST_Pin TEACH_SLOW_Pin CHECK_UP_Pin
+                           CHECK_DOWN_Pin */
+  GPIO_InitStruct.Pin = SAFETY_END_Pin|TEACH_FAST_Pin|TEACH_SLOW_Pin|CHECK_UP_Pin
+                          |CHECK_DOWN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : INSPECTION_MODE_Pin SPEED_READY_Pin SAFETY_OK_Pin STAND_OK_Pin
+                           ROTATION_OK_Pin */
+  GPIO_InitStruct.Pin = INSPECTION_MODE_Pin|SPEED_READY_Pin|SAFETY_OK_Pin|STAND_OK_Pin
+                          |ROTATION_OK_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : IN_FREQ_Pin */
+  GPIO_InitStruct.Pin = IN_FREQ_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_INPUT;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(IN_FREQ_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : WATCHDOG_OUT_Pin */
+  GPIO_InitStruct.Pin = WATCHDOG_OUT_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(WATCHDOG_OUT_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pin : K2_EN_Pin */
+  GPIO_InitStruct.Pin = K2_EN_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(K2_EN_GPIO_Port, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : ROTATION_S2_Pin MIS_ST2_Pin MIS_ST1_Pin */
+  GPIO_InitStruct.Pin = ROTATION_S2_Pin|MIS_ST2_Pin|MIS_ST1_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+
+}
+
+/* USER CODE BEGIN 2 */
+
+/* USER CODE END 2 */
